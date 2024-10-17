78c272494a8ebddd7acbd8e56c2affe84fbf1f88
Riquelme0800 <midorya2002@gmail.com>
2024-10-17 20:26:05 -0300

    Create Ex002Nivel2.py

---
+++ Ex002.py
@@ -0,0 +1,27 @@
+A1 ← (4/2)+(2/4)	
+4/2=2                      
+2/4=0.5
+2=0.5= 2.5
+A2 ← 4/2+2/4
+4/2=2                      
+2/4=0.5
+2=0.5= 2.5
+#Ambos produzem o resultado de "2.5"Então são iguais.
+
+B1 ← 4/(2+2)/4	
+2+2=4
+4/4=1
+B2 ← 4/2+2/4
+4/2=2
+2/4=0.5
+2+0.5=2.5
+#B1 produz 1 e B2 produz 2.5, então são diferentes. 
+
+C1 ← (4+2)*2-4	
+4+2=6
+6*2=12
+12-4=8
+C2 ← 4+2*2-4
+2*2=4
+4+4-4=4
+#C1 produz 8 e C2 produz 4, então são diferentes. A diferença se deve à precedência das operações: em C1, a soma é realizada antes da multiplicação, enquanto em C2, a multiplicação é realizada antes da soma.
