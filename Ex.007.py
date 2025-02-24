20792a274c92b2e110f82783e1869ba201b39cb9
Riquelme0800 <midorya2002@gmail.com>
2025-02-24 19:47:39 -0300

    Create Ex.007.py

---
+++ Ex.007.py
@@ -0,0 +1,4 @@
+totalEleitores = int (input("qual o total de eleitores?"))
+votosbrancos = int(input("qual o total de votos brancos?"))
+votosValidos = int(input("qual o total de votos válidos?"))
+votosValidos = int(input("qual o total de votos nulos?"))
